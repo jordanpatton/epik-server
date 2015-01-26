4308399fe1eca07a82559f55e48d69a75566020b
Jordan Patton <jordanpatton@gmail.com>
2015-01-25 19:03:18 -0500

    Removed sensitive information.

--- config/secrets.js
+++ config/secrets.js
@@ -19,13 +19,13 @@
 module.exports = {
 
   //db: process.env.MONGODB|| 'mongodb://localhost:27017/epik',
-  db: process.env.MONGODB|| '***REMOVED***',
+  db: process.env.MONGODB|| '**REMOVED**',
 
-  sessionSecret: process.env.SESSION_SECRET || '***REMOVED***',
+  sessionSecret: process.env.SESSION_SECRET || '**REMOVED**',
 
   mailgun: {
-    user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
-    password: process.env.MAILGUN_PASSWORD || '29eldds1uri6'
+    user: process.env.MAILGUN_USER || '**REMOVED**',
+    password: process.env.MAILGUN_PASSWORD || '**REMOVED**'
   },
   
   mandrill: {
